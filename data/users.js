d0ea7279f5657be19b6f2fbae83004901e66c218
Lailatu22 <abubakarilailat@gmail.com>
2022-04-07 15:50:43 +0100

    first commit

---
+++ data/users.js
@@ -0,0 +1,11 @@
+const users=[
+    {id:1, name:"John", age:25},
+    {id:2, name:"Peter",age:27},
+    {id:3, name:"Rufia",age:22},
+    {id:4, name:"Andy",age:20},
+    {id:5, name: "Rashed",age:28},
+
+    
+
+]
+module.exports=users
