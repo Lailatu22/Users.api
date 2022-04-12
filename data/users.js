de5f59ac2fc8c453bf0d8ae1c94312c113b0529f
Lailatu22 <abubakarilailat@gmail.com>
2022-04-12 15:35:11 +0100

    added middleware

--- data/users.js
+++ data/users.js
@@ -1,11 +1,8 @@
 const users=[
-    {id:1, name:"John", age:25},
-    {id:2, name:"Peter",age:27},
-    {id:3, name:"Rufia",age:22},
-    {id:4, name:"Andy",age:20},
-    {id:5, name: "Rashed",age:28},
-
-    
-
+    {id:1, name:"James", age:18},
+    {id:2, name:"Jennifer", age:20},
+    {id:3, name:"Muniba", age:29},
+    {id:4, name:"Umietu", age:30},
+    {id:5, name:"Maxima", age:19}
 ]
 module.exports=users
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express =require("express")
-const usersRoutes = require("./routes/usersRoutes")
+const userRoute = require("./routes/usersRoutes")
 const path = require("path")
+const logger=require("./middlewares/user.logger")
+const booksRoute = require("./routes/booksRoute")
 
 const app = express()
 
-//middlewares
+//middlewires
 app.use(express.json());
-
-app.use(usersRoutes)
+app.use(logger)
+app.use(userRoute)
+app.use(booksRoute)
 
 //home route
 app.get("/", (req, res)=>{
-res.sendFile(path.join(__dirname + "/pages/index.html"))    
+    res.status(200).send("<h1>WELCOME TO MY USERS DATABASE</h1>");
 })
+
 const PORT = 4000;
 
+
 app.listen(PORT, ()=>{
-    console.log("SERVER IS UP")
+console.log("SERVER IS UP")
+
 })
